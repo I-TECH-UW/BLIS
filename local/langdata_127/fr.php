3d42546f04269dafe75a2eeaa0552380985f144b
Ben Smith <bens23@uw.edu>
2017-05-11 10:38:06 -0700

    fix for PT issue #145227135 - French Pages are not rendering
    https://www.pivotaltracker.com/n/projects/1100872

--- local/langdata_127/fr.php
+++ local/langdata_127/fr.php
@@ -480,8 +480,8 @@ $LANG_ARRAY = array (
         "ENABLE_SITE_ENTRY" => "Activer l'entrée du site pour un spécimen",
         "SELECT_SITE_FOR_REMOVAL" => "Sélection de sites pour l'élimination",
 		 "OUT_OF_RANGE" => "hors de portée",
-        "INTERPRETATION" => "Interprétation"
-        "MODIFY_SITE" => "Modifier les sites"
+        "INTERPRETATION" => "Interprétation",
+        "MODIFY_SITE" => "Modifier les sites",
 
     ) ,
 	"lab_admins" => array ( 
